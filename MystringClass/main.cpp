f4cb4d343a4af5d10250a5316b602ae149f5145f
nisanth mathew james <nisanthmathewjames@gmail.com>
2020-03-11 09:51:07 +0100

    added simple code to initialize and display MyString

--- MystringClass/main.cpp
+++ MystringClass/main.cpp
@@ -1,8 +1,12 @@
 #include <QCoreApplication>
+#include "mystring.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    MyString HelloWorld{"Hello World!!!"};
+    std::cout << HelloWorld.getmystring() << std::endl;
 
     return a.exec();
 }
